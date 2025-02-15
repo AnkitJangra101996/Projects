4d6f3a40be06a8adc2f9112d037a44c2bc06bef6
Ankit Jangra <ankitjangra101996@gmail.com>
2025-02-15 10:22:28 +0530

    Some changes in app file

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,11 +1,23 @@
 import { Button } from "rsuite";
+import { createPortal } from "react-dom";
+import { useState } from "react";
 
 const App = () => {
-  return (<>
-    <Button>Hello World</Button>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1></>
+  const [show, setShow] = useState(false);
+
+  return (
+    <>
+      <Button className="button" onClick={() => setShow(true)}>Hello World</Button>
+      {show &&
+        createPortal(
+          <>
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            <p>This child is placed in the document body.</p>
+            <button onClick={() => setShow(false)}>close</button>
+          </>,
+          document.body
+        )}
+    </>
   );
 };
 
