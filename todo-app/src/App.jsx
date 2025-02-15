bbf6ccf73510188dc79ec2b6f221de8b553ec87b
Ankit Jangra <ankitjangra101996@gmail.com>
2025-02-15 09:54:07 +0530

    some work done

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,7 +1,12 @@
 import { Button } from "rsuite";
 
 const App = () => {
-  return <Button>Hello World</Button>;
+  return (<>
+    <Button>Hello World</Button>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1></>
+  );
 };
 
 export default App;
